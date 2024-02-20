12efeaf9a06c4607fec086bd19ab466b7af92eb0
Jodi Kraushar <jodi.kraushar@kellogg.northwestern.edu>
2024-02-20 05:10:11 +0000

    part 1 complete hw3

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,11 +10,11 @@ class PlacesController < ApplicationController
   def new
     @place = Place.new
   end
-  
+
   def create
-    @place = Place.new(params["place"])
+    @place = Place.new
+    @place["place"] = params["place"]
     @place.save
     redirect_to "/places"
   end
-  
 end
